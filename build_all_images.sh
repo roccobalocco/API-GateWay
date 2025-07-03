8c575ecff3142c7161f843fa9cda82a02fdd2606
roccobalocco <pietro.masolini@studenti.unimi.it>
2025-07-03 17:50:33 +0200

    continuazione docker e kubernetes

--- build_all_images.sh
+++ build_all_images.sh
@@ -1,9 +1,13 @@
-(sudo docker buildx build -f Dockerfile.gateway -t api-gateway .  > api-gateway.out) &
+(sudo docker buildx build -f Dockerfile.gateway -t api-gateway .  2> api-gateway.out && sudo docker tag api-gateway roccobalocco/api-gateway) &
 
-(sudo docker buildx build -f Dockerfile.user -t user . > user.out) &
+(sudo docker buildx build -f Dockerfile.user -t user . 2> user.out && sudo docker tag user roccobalocco/user) &
 
-(sudo docker buildx build -f Dockerfile.book -t book . > book.out) &
+(sudo docker buildx build -f Dockerfile.book -t book . 2> book.out && sudo docker tag book roccobalocco/book) &
 
-(sudo docker buildx build -f Dockerfile.loan -t loan . > loan.out) &
+(sudo docker buildx build -f Dockerfile.loan -t loan . 2> loan.out && sudo docker tag loan roccobalocco/loan) &
 
-(sudo docker buildx build -f Dockerfile.room -t room . > room.out) &
+(sudo docker buildx build -f Dockerfile.room -t room . 2> room.out && sudo docker tag room roccobalocco/room) &
+
+
+
+
